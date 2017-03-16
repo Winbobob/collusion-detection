9f69bbad268835eec0cc5437f4c3caea9fc3b337
Winbobob <zhu6@ncsu.edu>
2017-03-15 22:17:09 -0400

    Finish refactoring

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-var scoreSection = ['twofive', 'fivezero', 'sevenfive', 'onezerozero'];
 var classSection = ['link twofive', 'link fivezero', 'link sevenfive', 'link onezerozero'];
-
 var filepaths = ['collusion_detection_sample_724.txt', 
                  'collusion_detection_sample_733_wikib.txt', 
                  'collusion_detection_sample_749_final.txt',
@@ -53,7 +51,7 @@ links.forEach(function(link) {
 	}
 });
     
-// build the arrow. ???
+// draw the arrow.
 // Different link/path types can be defined here
 svg.append('svg:defs').selectAll('marker').data(['end'])
     .enter().append('svg:marker')
@@ -68,7 +66,7 @@ svg.append('svg:defs').selectAll('marker').data(['end'])
     .append('svg:path')
         .attr('d', 'M0,-5L10,0L0,5');
 
-// add the links and the arrows
+// define the paths (links + arrows)
 var path = svg.append('svg:g').selectAll('path').data(links)
     .enter().append('path')
         .attr('class', function(d) { return 'link ' + d.type; })
@@ -94,7 +92,7 @@ node.append('circle')
 node.append('text')
     .text(function(d) { return d.id; })
     .attr('x', 12)
-    .attr('text-anchor', 'beginning')
+    .attr('text-anchor', 'beginning');
     
 // Draw link curves and arrows
 // Use elliptical arc path segments to doubly-encode directionality.
@@ -149,35 +147,34 @@ node.on('click', function(d) {
         d3.select(this).select('text')
             .call(function (d) { unityId = d.text(); })
         d3.selectAll('path').filter(function(path) {
-                p = d3.select(this);
-                if(p.attr('reviewer_actor_id') === unityId ||
-                   p.attr('reviewee_actor_id') === unityId) {
-                    className = p.attr('class');
-                    if(className && classSection.indexOf(className) === -1) {
-                        d3.select(this)
-                            .classed(className, false)
-                            .classed(className.substring(0, className.lastIndexOf(' ')) + ' highlight', true)
-                    }
-                    else {
-                        d3.select(this)
-                            .classed(className, false)
-                            .classed(className + ' highlight', true)
-                    }
+            p = d3.select(this);
+            if(p.attr('reviewer_actor_id') === unityId ||
+               p.attr('reviewee_actor_id') === unityId) {
+                className = p.attr('class');
+                if(className && classSection.indexOf(className) === -1) {
+                    d3.select(this)
+                        .classed(className, false)
+                        .classed(className.substring(0, className.lastIndexOf(' ')) + ' highlight', true)
                 }
-                else{
-                    className = p.attr('class');
-                    if(className && classSection.indexOf(className) === -1) {
-                        d3.select(this)
-                            .classed(className, false)
-                            .classed(className.substring(0, className.lastIndexOf(' ')) + ' hidden', true)
-                    }
-                    else {
-                        d3.select(this)
-                            .classed(className, false)
-                            .classed(className + ' hidden', true)
-                    }
+                else {
+                    d3.select(this)
+                        .classed(className, false)
+                        .classed(className + ' highlight', true)
                 }
-            
+            }
+            else{
+                className = p.attr('class');
+                if(className && classSection.indexOf(className) === -1) {
+                    d3.select(this)
+                        .classed(className, false)
+                        .classed(className.substring(0, className.lastIndexOf(' ')) + ' hidden', true)
+                }
+                else {
+                    d3.select(this)
+                        .classed(className, false)
+                        .classed(className + ' hidden', true)
+                }
+            }    
         }) 
     }
     else {
@@ -205,142 +202,26 @@ node.on('click', function(d) {
     }
 })
     
-
-
-
-// not checked yet
-// https://bl.ocks.org/mbostock/4062045
-
-
-// Highlight node/text/in edge function
-function highlight_in() {
-    d3.select(this).select('text').transition()
-        .duration(750)
-        .attr('x', 22)
-        .style('fill', 'blue')
-        .style('stroke', 'blue')
-        .style('stroke-width', '.5px')
-        .style('font', '20px sans-serif');
-//    d3.select(this).select('circle').transition()
-//        .duration(750)
-//        .attr('r', 12)
-//        .style('fill', 'blue');
-    d3.select(this).select('text')
-        .call(function(d){
-            unityid = d[0][0].innerHTML;
-            d3.selectAll('path')[0].filter(function(path){
-                p=d3.select(path)
-                postfix=/[a-z]+$/.exec(p[0][0].attributes[0].nodeValue)[0] // obtain path status
-                if(p.attr('reviewee_actor_id')==unityid){
-                    if(postfix=='hidden'){
-                        p[0][0].attributes[0].nodeValue=p[0][0].attributes[0].nodeValue.substring(0,p[0][0].attributes[0].nodeValue.lastIndexOf(' '));
-                        postfix=/[a-z]+$/.exec(p[0][0].attributes[0].nodeValue)[0]
-                    }
-                    if(postfix=='twofive'||postfix=='fivezero'||postfix=='sevenfive'||postfix=='onezerozero'){
-                        p[0][0].attributes[0].nodeValue=p[0][0].attributes[0].nodeValue+' highlight';
-                    }
-                }else{
-                    if(postfix=='twofive'||postfix=='fivezero'||postfix=='sevenfive'||postfix=='onezerozero'){
-                        p[0][0].attributes[0].nodeValue=p[0][0].attributes[0].nodeValue+' hidden';
-                    }
+// for strong/weak submissions and easy/critical reviewers
+// call function 'highlight_nodes_and_paths'
+function show_diff_mode_type(mode, review_id_type, regex, threshold){
+    d3.selectAll('.node')
+    .call(function(d){
+        d[0].forEach(function(n){
+            paths = d3.selectAll('[' + review_id_type + ' = ' + n.__data__.id + ']');
+            selected_paths = paths[0].filter(function(d){ return d.attributes[0].nodeValue.match(regex); })
+            if(paths[0].length > 0){
+                percentage = Math.floor(selected_paths.length / paths[0].length);
+                if(percentage > threshold){
+                    console.log('=====' + mode + ' Submissions=====');
+                    console.log(n.__data__.id + '  Percentage: ' + percentage);
+                    highlight_nodes_and_paths(review_id_type, n.__data__.id);
                 }
-                
-            })
-        })
-}
-    
-// Unhighlight node/text/in edge function
-function unhighlight_in() {
-    d3.select(this).select('text').transition()
-        .duration(750)
-        .attr('x', 12)
-        .style('fill', 'black')
-        .style('stroke', 'none')
-        .style('stroke-width', '.5px')
-        .style('font', '10px sans-serif');
-//    d3.select(this).select('circle').transition()
-//        .duration(750)
-//        .attr('r', 6)
-//        .style('fill', '#ccc');
-    d3.select(this).select('text')
-        .call(function(d){ 
-            unityid = d[0][0].innerHTML;
-            d3.selectAll('path')[0].filter(function(path){
-                p=d3.select(path)
-                postfix=/[a-z]+$/.exec(p[0][0].attributes[0].nodeValue)[0]
-                if(postfix=='hidden' || postfix=='highlight'){
-                    p[0][0].attributes[0].nodeValue=p[0][0].attributes[0].nodeValue.substring(0,p[0][0].attributes[0].nodeValue.lastIndexOf(' '));
-                }
-            })
-        })
+            }
+        });
+    });
 }
 
-// Highlight node/text/in edge function
-function highlight_out() {
-    d3.select(this).select('text').transition()
-        .duration(750)
-        .attr('x', 22)
-        .style('fill', 'blue')
-        .style('stroke', 'blue')
-        .style('stroke-width', '.5px')
-        .style('font', '20px sans-serif');
-//    d3.select(this).select('circle').transition()
-//        .duration(750)
-//        .attr('r', 16)
-//        .style('fill', 'blue');
-    d3.select(this).select('text')
-        .call(function(d){ 
-            unityid = d[0][0].innerHTML;
-            // console.log(unityid);
-            d3.selectAll('path')[0].filter(function(path){
-                p=d3.select(path)
-                postfix=/[a-z]+$/.exec(p[0][0].attributes[0].nodeValue)[0]
-                if(p.attr('reviewer_actor_id')==unityid){
-                    if(postfix=='hidden'){
-                        p[0][0].attributes[0].nodeValue=p[0][0].attributes[0].nodeValue.substring(0,p[0][0].attributes[0].nodeValue.lastIndexOf(' '));
-                        postfix=/[a-z]+$/.exec(p[0][0].attributes[0].nodeValue)[0]
-                    }
-                    if(postfix=='twofive'||postfix=='fivezero'||postfix=='sevenfive'||postfix=='onezerozero'){
-                        p[0][0].attributes[0].nodeValue=p[0][0].attributes[0].nodeValue+' highlight';
-                    }
-                }else{
-                    if(postfix=='twofive'||postfix=='fivezero'||postfix=='sevenfive'||postfix=='onezerozero'){
-                        p[0][0].attributes[0].nodeValue=p[0][0].attributes[0].nodeValue+' hidden';
-                    }
-                }
-            })
-        })
-}
-    
-// Unhighlight node/text/in edge function
-//function unhighlight_out() {
-//    d3.select(this).select('text').transition()
-//        .duration(750)
-//        .attr('x', 12)
-//        .style('fill', 'black')
-//        .style('stroke', 'none')
-//        .style('stroke-width', '.5px')
-//        .style('font', '10px sans-serif');
-//    d3.select(this).select('circle').transition()
-//        .duration(750)
-//        .attr('r', 6)
-//        .style('fill', '#ccc');
-//    d3.select(this).select('text')
-//        .call(function(d){ 
-//            unityid = d[0][0].innerHTML;
-//            // console.log(unityid);
-//            d3.selectAll('[reviewer_actor_id='+unityid+']')
-//                .call(function(d){ 
-//                    d[0].forEach(function(p){
-//                        if(p.attributes[0].nodeValue.substring(p.attributes[0].nodeValue.lastIndexOf(' ')+1)=='highlight'){
-//                            p.attributes[0].nodeValue=p.attributes[0].nodeValue.substring(0,p.attributes[0].nodeValue.lastIndexOf(' '));
-//                        }
-//                    });
-//                })
-//        })
-//}
-    
-    
 // for strong/weak submissions and easy/critical reviewers
 function highlight_nodes_and_paths(review_id_type, id){
     // highlight related nodes (text and circle)
@@ -445,86 +326,19 @@ function unhighlight_all_nodes_and_paths(){
 }
 
 // Show different graphs according to top radio button selection
-d3.selectAll('#horizon-controls input[name=mode]').on('change', function() {
-//    // Before highlight collusion links
-//    // Change existing 'highlight' attr to 'hidden' or add 'hidden' attr
-//    d3.selectAll('path')[0].filter(function(path){
-//        p=d3.select(path);
-//        nodeValue=/[a-z]+$/.exec(p[0][0].attributes[0].nodeValue);
-//        if(nodeValue && nodeValue.length > 0){
-//            postfix=nodeValue[0];
-//            if(postfix == 'highlight'){
-//                p[0][0].attributes[0].nodeValue.replace('highlight', 'hidden');
-//            }else if(scoreSection.indexOf(postfix) !== -1){
-//                p[0][0].attributes[0].nodeValue += ' hidden';
-//            }
-//        }
-//    })
-        
+d3.selectAll('#horizon-controls input[name=mode]').on('change', function() {        
     if(this.value == 'strong'){
-        d3.selectAll('.node')
-            .call(function(d){
-                d[0].forEach(function(n){
-                    paths = d3.selectAll('[reviewee_actor_id = ' + n.__data__.id + ']');
-                    strong_paths = paths[0].filter(function(d){ return d.attributes[0].nodeValue.match(/onezerozero/); })
-                    if(paths[0].length > 0){
-                        strong_percentage = Math.floor(strong_paths.length / paths[0].length);
-                        if(strong_percentage > 0.8){
-                            console.log('=====Strong Submissions=====');
-                            console.log(n.__data__.id + '  Percentage: ' + strong_percentage);
-                            highlight_nodes_and_paths('reviewee_actor_id', n.__data__.id);
-                        }
-                    }
-                });
-            });
+        unhighlight_all_nodes_and_paths();
+        show_diff_mode_type(this.value, 'reviewee_actor_id', /onezerozero/, 0.8);
     }else if(this.value == 'weak'){
-        d3.selectAll('.node')
-            .call(function(d){
-                d[0].forEach(function(n){
-                    paths = d3.selectAll('[reviewee_actor_id = ' + n.__data__.id + ']');
-                    weak_paths = paths[0].filter(function(d){ return d.attributes[0].nodeValue.match(/(fivezero|twofive|sevenfive)/); })
-                    if(paths[0].length > 0){
-                        weak_percentage = Math.floor(weak_paths.length / paths[0].length);
-                        if(weak_percentage > 0.8){
-                            console.log('=====Weak Submissions=====');
-                            console.log(n.__data__.id + '  Percentage: ' + weak_percentage);
-                            highlight_nodes_and_paths('reviewee_actor_id', n.__data__.id);
-                        }
-                    }
-                });
-            });
+        unhighlight_all_nodes_and_paths();
+        show_diff_mode_type(this.value, 'reviewee_actor_id', /(fivezero|twofive|sevenfive)/, 0.8);
     }else if(this.value == 'easy'){
-        d3.selectAll('.node')
-            .call(function(d){
-                d[0].forEach(function(n){
-                    paths = d3.selectAll('[reviewer_actor_id = ' + n.__data__.id + ']');
-                    easy_paths = paths[0].filter(function(d){ return d.attributes[0].nodeValue.match(/onezerozero/); })
-                    if(paths[0].length > 0){
-                        easy_percentage = Math.floor(easy_paths.length / paths[0].length);
-                        if(easy_percentage > 0.8){
-                            console.log('=====Easy-going Submissions=====');
-                            console.log(n.__data__.id + '  Percentage: ' + easy_percentage);
-                            highlight_nodes_and_paths('reviewer_actor_id', n.__data__.id);
-                        }
-                    }
-                });
-            });
+        unhighlight_all_nodes_and_paths();
+        show_diff_mode_type(this.value, 'reviewer_actor_id', /onezerozero/, 0.8);
     }else if(this.value == 'critical'){
-        d3.selectAll('.node')
-            .call(function(d){
-                d[0].forEach(function(n){
-                    paths=d3.selectAll('[reviewer_actor_id = ' + n.__data__.id + ']');
-                    critial_paths = paths[0].filter(function(d){ return d.attributes[0].nodeValue.match(/(fivezero|twofive)/); })
-                    if(paths[0].length > 0){
-                        critial_percentage = Math.floor(critial_paths.length / paths[0].length);
-                        if(critial_percentage > 0.9){
-                            console.log('=====Critial Submissions=====');
-                            console.log(n.__data__.id + '  Percentage: ' + critial_percentage);
-                            highlight_nodes_and_paths('reviewer_actor_id', n.__data__.id);
-                        }
-                    }
-                });
-            });
+        unhighlight_all_nodes_and_paths();
+        show_diff_mode_type(this.value, 'reviewer_actor_id', /(fivezero|twofive)/, 0.9);
     }else if(this.value == 'colludes'){
         coll_cyc.forEach(function(coll){
             c=coll.colluders
